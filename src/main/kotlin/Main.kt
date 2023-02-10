c28b0518e71e17d07c461ddda38c7dca79dcc4b5
Mauro L <mlizaur@eventbrite.com>
2023-02-10 17:43:27 -0300

    Adding week 1 exercises

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,44 @@
+import week_1.Status
+import week_1.getTicketOrNull
+import week_1.printTicketOrNotFound
+import java.time.LocalDate
+
 fun main(args: Array<String>) {
-        println("Hello world")
-    }
+    println ("Exercise 1")
+    val reversed = week_1.rev("hola")
+    println ( "word -> hola | rev -> $reversed")
+    // ---
+    println ("Exercise 2")
+    val wordSize = week_1.sizes("murcielago")
+    println("word -> murcielago | size -> $wordSize")
+    // ---
+    println ("Exercise 3")
+    val ticket = week_1.Ticket(id="123", value=23, door="B 4")
+    println("Ticket -> $ticket")
+    // ---
+    println ("Exercise 4")
+    val logger = week_1.Logger
+    logger.debug("hola mundo")
+    logger.warning("cuidado mundo")
+    logger.error("se rompio el mundo")
+    // ---
+    println ("Exercise 5")
+    val status_ok = Status.APPROVED
+    val status_pend = Status.PENDING
+    val status_nope = Status.DENIED
+    println ("Status OK -> ${status_ok.toString()}")
+    println ("Status WAIT -> ${status_pend.toString()}")
+    println ("Status NO -> ${status_nope.toString()}")
+    // ---
+    println ("Exercise 6")
+    val now = LocalDate.now()
+    val yesterday = LocalDate.now().minusDays(1)
+    val ticket_ok = getTicketOrNull(now)
+    val ticket_no = getTicketOrNull(yesterday)
+    println ("Ticket OK -> $ticket_ok")
+    println ("Ticket NULL -> $ticket_no")
+    // ---
+    println ("Exercise 7")
+    printTicketOrNotFound(ticket_ok)
+    printTicketOrNotFound(ticket_no)
+}
---
+++ src/main/kotlin/week_1/Exercise1.kt
@@ -0,0 +1,86 @@
+package week_1
+
+import java.time.LocalDate
+
+//1. Create a function that receives a string and reverse it
+fun rev(value: String): String = value.reversed()
+
+
+//2. Create a function that receives a string and:
+//* Prints `short` if the string has a length less than 5
+//* Prints `medium` if the string has a length between 5 and 10
+//* Prints `long` if the string has a length more than 10
+fun sizes(value: String): String {
+    val valueLength: Int = value.length
+    return when {
+        valueLength < 5 -> "short"
+        valueLength in 5..10 -> "medium"
+        else -> "long"
+    }
+}
+
+
+//3. Create a Ticket class with the following attributes
+//* id -> string
+//* purchase value -> int
+//* event date -> date
+//* door -> string
+class Ticket(private val id: String, val value:Int, val eventDate: LocalDate? = LocalDate.now(), val door: String) {
+    fun getId(): String {
+        return "EVB-${this.id}"
+    }
+}
+
+
+//4. Create an object that handles logging
+enum class LogLevel {
+    DEBUG,
+    WARNING,
+    ERROR
+}
+
+object Logger {
+    private fun log(level: LogLevel, message: String) {
+        val lvlName = level.name
+        println("[$lvlName] $message")
+    }
+
+    fun debug(message: String) = this.log(LogLevel.DEBUG, message)
+    fun warning(message: String) = this.log(LogLevel.WARNING, message)
+    fun error(message: String) = this.log(LogLevel.ERROR, message)
+}
+
+
+//5. Create an enum class for different event statuses
+//* Handle the event statuses in a when expression
+//* Obtain an event status from a string
+enum class Status {
+    APPROVED,
+    PENDING,
+    DENIED;
+
+    override fun toString() : String {
+        return when (this) {
+            APPROVED -> "Aprobado"
+            PENDING -> "Pendiente"
+            DENIED -> "Denegado"
+        }
+    }
+}
+
+
+//6. Create a function that returns a ticket instance given a condition or null if that condition is not achieved
+fun getTicketOrNull (eventDate: LocalDate) : Ticket? {
+    return if (eventDate < LocalDate.now()) {
+        null
+    } else {
+        Ticket("123", 23, LocalDate.now().plusDays(1),"12 A" )
+    }
+}
+
+//7. Create a function that handles the previous function and print the ticket information or print `NOT_FOUND`
+fun printTicketOrNotFound(ticket: Ticket?) = if (ticket != null) {
+    println("[TICKET -> ${ticket.getId()}] Event Date: ${ticket.eventDate} - Door: ${ticket.door}")
+} else {
+    print ("NOT_FOUND")
+}
