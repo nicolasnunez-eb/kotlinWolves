e7f8990000ab04a87f64f2c1597cac1f90b94031
nicolas.nunez <nicolas.nunez@eventbrite.com>
2023-02-23 16:11:07 -0300

    Add null examples

--- src/main/kotlin/week_1/5_ExampleNulls.kt
+++ src/main/kotlin/week_1/5_ExampleNulls.kt
@@ -1,1 +1,17 @@
 package week_1
+
+/*
+    Kotlin has null safety which means that types by default cannot contain null values.
+    This makes our life easier and our code will avoid those horribles NullPointerException.
+
+    If we know that something can contain a null value we have to make it explicit in our type system.
+    This comes with the syntax `?` in our types. If we add `?` at the end of our types we are saying that
+    our variable can contain a null value.
+*/
+
+fun nullExample(number: Int?): Int {
+//    return number + 10 # We can't do this, the compiler alert us that number can be null.
+    if (number == null)
+        throw IllegalArgumentException("Number is null")
+    return number + 10
+}
